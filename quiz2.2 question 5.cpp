6253c8a3a081fca5ef5f80fa745091e7cfaf6300
MarcoCota <Hello_Andrew_Cota@yahoo.com>
2021-03-23 03:40:10 +0000

    change

---
+++ quiz2.2 question 5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int num, i;
+    int value1, value2;
+
+    cout << "Enter your desired range of values : ";
+    cin >> value1 >> value2;
+
+    for(num=value1; num <= value2; num++)
+    
+}
