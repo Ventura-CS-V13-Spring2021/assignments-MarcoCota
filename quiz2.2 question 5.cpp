57885ed5c5397b60b9198f79d0699c864e1f8b1a
MarcoCota <Hello_Andrew_Cota@yahoo.com>
2021-03-23 03:45:55 +0000

    end of question 5

--- quiz2.2 question 5.cpp
+++ quiz2.2 question 5.cpp
@@ -7,9 +7,19 @@ int main()
     int num, i;
     int value1, value2;
 
-    cout << "Enter your desired range of values : ";
+    cout << "Enter a start and ending range : ";
     cin >> value1 >> value2;
 
     for(num=value1; num <= value2; num++)
-    
+    {
+        for(i=2; i<num; i++)
+        {
+            if(num % i ==0)
+                break;
+        }
+        if(i == num)
+            cout << num << " This is a prime value within your chosen range : ";
+        else
+            cout << num << " This is not a prime value within chosen range : ";
+    }
 }
