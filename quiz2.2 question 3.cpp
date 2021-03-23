895c0e948840e0e0d80b89b652516e62012b637c
MarcoCota <Hello_Andrew_Cota@yahoo.com>
2021-03-23 03:20:38 +0000

    question 3 continued

--- quiz2.2 question 3.cpp
+++ quiz2.2 question 3.cpp
@@ -5,6 +5,18 @@ int main()
 {
    //creats the input range of 0-99
     int int1, int2, int3;
+    cout << "Input three integers in the range of 0-99" << endl;
+    cin >> int1 >> int2 >> int3;
 
+    if (int1 == int2)
+    {
+        if (int1 == int3)
+            cout << "There are duplicate numbers" << endl;
+          // int1 is not equal to int3
+        else
+            cout << "int1 and in2 are duplicates" << endl;
+    }
+    else 
+    
     
 
