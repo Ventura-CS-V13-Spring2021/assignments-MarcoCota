60ea15575548cea6a550d4c1829f32dbf2a76ad8
MarcoCota <Hello_Andrew_Cota@yahoo.com>
2021-03-23 03:56:51 +0000

    I do not know how to stop from rewriting each cpp I am creating for each question but it keeps overwriting the code I type in when I do a new question

--- quiz2-2.cpp
+++ quiz2-2.cpp
@@ -1,4 +1,12 @@
-#include <iosstream
+#include <iosstream>
+#include <ctime>
 
 using namespace std;
 
+int main()
+{
+    srand(time(NULL));
+    //Tells the code to pick a number between zero and 1 hundred
+    cout << rand()%100;
+
+}
