19ab50fbffc37b6c93b88324e833114224ae9173
MarcoCota <>
2021-04-20 22:53:59 +0000

    cont question 1

--- Midterm/question 1.cpp
+++ Midterm/question 1.cpp
@@ -8,3 +8,10 @@ int main()
 {
   ifstream infile;
   infile.open("students.txt"); // allows program to interact with txt file
+
+  //check for error
+  if (infile.fail())
+  {
+    cout << "error opening file" << endl;
+    exit(1); // stops programs when 1 error occurs
+  }
