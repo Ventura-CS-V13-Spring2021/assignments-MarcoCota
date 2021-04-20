c12a37229612e59cf92da804cc0bb3effa60f062
MarcoCota <>
2021-04-20 22:42:05 +0000

    Continuing question 1

---
+++ Midterm/question 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <string> 
+
+using namespace std;
+
+int main()
+{
+  ifstream infile;
+  infile.open("students.txt"); // allows program to interact with txt file
