29103ab6fa1862c1c551ef243a25a81e9f93e09b
MarcoCota <>
2021-04-20 23:04:19 +0000

    cont qeustion 1

--- Midterm/question 1.cpp
+++ Midterm/question 1.cpp
@@ -15,3 +15,13 @@ int main()
     cout << "error opening file" << endl;
     exit(1); // stops programs when 1 error occurs
   }
+
+ int x, y;
+    
+    infile >> x >> y; // takes in two test values for each student
+    
+    cout << "num1 :" << x << endl;
+    cout << "num2 :" << y << endl;
+    
+    return 0;
+}
