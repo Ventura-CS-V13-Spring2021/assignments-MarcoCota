70f6be73538e3e0ab8dd5f9ac44a11afd8afd922
MarcoCota <>
2021-04-21 01:20:00 +0000

    cont question 3

--- Midterm/question 3.cpp
+++ Midterm/question 3.cpp
@@ -21,3 +21,32 @@ int getRdnum(void)
     value = rand() % 10;  // selects a random number from 0 to 9
     return value;
 }
+int findMin(int num1, int num2, int num3) // determines which num is the min value
+{
+
+  int min = 10; // forces code to not go any higher than 9
+
+  if (num1 < min)
+    min=num1;
+  if (num2 < min)
+    min=num2;
+  if(num3 < min)
+    min=num3;
+
+  return min;
+}
+
+int findMax(int num1, int num2, int num3) // determines which num is the max value
+{
+
+  int max = -1; // forces code to not go any lower than 0
+
+  if (num1 > max)
+    max=num1;
+  if (num2 > max)
+    max=num2;
+  if(num3 > max)
+    max=num3;
+
+  return max;
+}
