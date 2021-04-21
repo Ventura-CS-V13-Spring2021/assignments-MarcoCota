219531f9d2efef96f0a3d7c05884df2a8e7c012a
MarcoCota <>
2021-04-21 00:09:06 +0000

    cont question 2

--- Midterm/question 2.cpp
+++ Midterm/question 2.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 int main()
 {
-    int value = 0;
+    int value = 0; // starts value at 0
     ofstream outfile;
-    outfile.open("numbers.txt");
+    outfile.open("numbers.txt"); // interacts with numbers text file
+
+    for (int i = 0; i < 10; i++) // prints 10 numbers at random 
+    {
+        value = getRdnum();
+        outfile << i << " " << value << endl;
+    }
