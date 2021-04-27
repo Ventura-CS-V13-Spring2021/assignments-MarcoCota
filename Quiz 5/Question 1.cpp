df6d30fb6c79714157381134a3bf40fd989fd899
MarcoCota <>
2021-04-27 05:31:49 +0000

    question 1 cont

--- Quiz 5/Question 1.cpp
+++ Quiz 5/Question 1.cpp
@@ -7,4 +7,10 @@ int main()
     int     numbers[10] = {5, 10, 75, 80, 25, 55, 95, 70, 60, 15};
     int     diff[10];
     int     sum=0;
-   
+   for (int i = 0; i<SIZE; i++)   // takes the elements in the array and adds them together
+    {
+        cout << numbers[i] << " "; // prints out each element in the array as they're being added
+        sum += numbers[i];
+    }
+    cout << endl;
+
