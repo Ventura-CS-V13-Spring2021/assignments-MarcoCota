c656aded68c4bf3473d9808a77dfa9a000154a40
MarcoCota <>
2021-05-14 06:25:56 +0000

    assignment 5-2 complete

--- Assignments before quiz 5/Assignment 5-2.cpp
+++ Assignments before quiz 5/Assignment 5-2.cpp
@@ -3,16 +3,24 @@
 #include <ctime>
 using namespace std;
 
+void makearray(int n[], int N); // forgot to add the prototypes before main 
+void printarray(int n[], int N); 
+void findminmax(int n[], int N); 
+
 int main()
 { 
-  const int N = 10; // sets the limit to max along with starting the value of max and min to 0
+    const int N = 10; // sets the limit to max along with starting the value of max and min to 0
     int numbers[N];
-    int nMax = 0;
-    int nMin = 0;
+    makearray(numbers,N);
+    printarray(numbers,N);
+    findminmax(numbers,N);
     
-void makearray(int n[], int N); 
+    return 0;
+} 
+    
+void makearray(int n[], int N) 
 {
-  int MAX = 100;
+    int MAX = 100;
     srand(time(NULL)); // allows code to provide random elements each time
     for (int i = 0; i<N; i++ )
     {
@@ -20,17 +28,19 @@ void makearray(int n[], int N);
     }
 }
 
-void printarray(int n[], int N); 
+void printarray(int n[], int N) 
 {
-    for (int i = 0; i<N; i++ ) // makes it so the array gives 10 elements in the array then prints each one out
+    cout << "List of elements: ";
+    for (int i = 0; i<N; i++ )// makes it so the array gives 10 elements in the array then prints each one out
     {
         cout << n[i] << " ";
     }
+    cout << endl;
 }
 
-void findminmax(int n[], int N); 
+void findminmax(int n[], int N) 
 {
-   int nMax = 0; // starts nMax at 0
+    int nMax = 0; // starts nMax at 0
     int nMin = 99; // starts nMax at 99
     for (int i = 0; i<N; i++ ) // goes through each element of the array
     {
