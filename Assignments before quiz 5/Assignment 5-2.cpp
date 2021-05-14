3932323fd9173d2251c75a4a6b6701e88b07ec2e
MarcoCota <>
2021-05-14 05:49:37 +0000

    cont 5-2

--- Assignments before quiz 5/Assignment 5-2.cpp
+++ Assignments before quiz 5/Assignment 5-2.cpp
@@ -5,3 +5,10 @@ using namespace std;
 
 int main()
 { 
+  const int N = 10; // sets the limit to max along with starting the value of max and min to 0
+    int numbers[N];
+    int nMax = 0;
+    int nMin = 0;
+    
+void makearray(int n[], int N); 
+{
