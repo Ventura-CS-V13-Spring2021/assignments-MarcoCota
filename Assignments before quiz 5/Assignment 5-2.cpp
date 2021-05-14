c842a10ac05453ad6fd938808ed3df0d0fcb06fb
MarcoCota <>
2021-05-14 06:19:58 +0000

    cont 5-2

--- Assignments before quiz 5/Assignment 5-2.cpp
+++ Assignments before quiz 5/Assignment 5-2.cpp
@@ -22,3 +22,28 @@ void makearray(int n[], int N);
 
 void printarray(int n[], int N); 
 {
+    for (int i = 0; i<N; i++ ) // makes it so the array gives 10 elements in the array then prints each one out
+    {
+        cout << n[i] << " ";
+    }
+}
+
+void findminmax(int n[], int N); 
+{
+   int nMax = 0; // starts nMax at 0
+    int nMin = 99; // starts nMax at 99
+    for (int i = 0; i<N; i++ ) // goes through each element of the array
+    {
+        if (n[i]>nMax) // checks if current elemenet becomes new nMax
+        {
+            nMax = n[i];
+        }
+        if (n[i]<nMin) // checks if current element becomes new nMin
+        {
+            nMin = n[i];
+        }
+    }
+    
+    cout << "This is the Max element: " << nMax << endl; // prints out the max element after going throught the array
+    cout << "This is the Min element: " << nMin << endl; // prints out the min element after going throught the array
+}
