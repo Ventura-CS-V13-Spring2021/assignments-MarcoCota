1bde68f0c64992104c3c638b8a15ce865ac9f482
MarcoCota <>
2021-05-17 07:13:46 +0000

    start of question 1

---
+++ Final/Question 1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int getNumDiv(int i);
+    
+    int i;
+    const int N = 10; // sets array to contain 10 elements
+    int array[10] = {1,5,6,10,20,30,40,50,55,60}; // array of 10 elements
+
