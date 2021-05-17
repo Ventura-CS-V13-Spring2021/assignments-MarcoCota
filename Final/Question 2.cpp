4d2733abf0321c3d3b58296a9b41f27bc50d758e
MarcoCota <>
2021-05-17 07:34:45 +0000

    start of question 2

---
+++ Final/Question 2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+  int i; 
+  cont int N = 20; // sets array to 20 elements max
+  int array[20] = {-12, 3, -12, 4, 1, 1, -12, 1, -1, 1, 2, 3, 4, 2, 3, -12, 4, 2, 1, 5};  
