93b85bf5a532c06ffd19416939673edb40581b64
MarcoCota <>
2021-05-17 07:55:28 +0000

    cont question 2

--- Final/Question 2.cpp
+++ Final/Question 2.cpp
@@ -3,17 +3,23 @@
 
 using namespace std;
 
-
 int main()
 {
-  int i; 
-  cont int N = 20; // sets array to 20 elements max
-  int array[20] = {-12, 3, -12, 4, 1, 1, -12, 1, -1, 1, 2, 3, 4, 2, 3, -12, 4, 2, 1, 5};  
-  sort(array, array + N, greater<int>()); // sorts elements in array from largest to smallest
+    int i; 
+    const int N = 20; // sets array to 20 elements max
+    int array[20] = {-12, 3, -12, 4, 1, 1, -12, 1, -1, 1, 2, 3, 4, 2, 3, -12, 4, 2, 1, 5};
+    sort(array, array + N, greater<int>()); // sorts elements in array from largest to smallest
     
+    cout << "N" << endl; // makes it so N is above the elements in the array vertically
+    cout << setw(5) << "Count" << endl;
     for (int i = 0; i<N; i++) // goes through each element in array
     {
+        if 
+        {
+          cout << array[i] << "  " << endl; // prints out elements from largest to smallest
+        
         
     }
+
     
 }
