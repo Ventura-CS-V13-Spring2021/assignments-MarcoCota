20048a34bb56794cb388ca54d9e98c4fd2fc848f
MarcoCota <>
2021-05-17 08:07:23 +0000

    fixed line 13 and deleted count in it's own line didn't put setw just put spaces infront of count instead.

--- Final/Question 2.cpp
+++ Final/Question 2.cpp
@@ -10,15 +10,15 @@ int main()
     int array[20] = {-12, 3, -12, 4, 1, 1, -12, 1, -1, 1, 2, 3, 4, 2, 3, -12, 4, 2, 1, 5};
     sort(array, array + N, greater<int>()); // sorts elements in array from largest to smallest
     
-    cout << "N" << endl; // makes it so N is above the elements in the array vertically
-    cout << setw(5) << "Count" << endl;
+    cout << "N" << "   Count" << endl; // makes it so N is above the elements in the array vertically
     for (int i = 0; i<N; i++) // goes through each element in array
     {
-        if 
+        if (array[i] == array[i-1]) // checks for duplicates with previous integer
         {
-          cout << array[i] << "  " << endl; // prints out elements from largest to smallest
+            
+            cout << array[i] << "  " << endl; // prints out elements from largest to smallest
         
-        
+        }
     }
 
     
