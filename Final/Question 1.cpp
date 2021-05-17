c370acb4f0f3729d78661f957dbe877aca3aa6b8
MarcoCota <>
2021-05-17 07:21:08 +0000

    input getnumdive function

--- Final/Question 1.cpp
+++ Final/Question 1.cpp
@@ -7,3 +7,18 @@ int getNumDiv(int i);
     const int N = 10; // sets array to contain 10 elements
     int array[10] = {1,5,6,10,20,30,40,50,55,60}; // array of 10 elements
 
+int main()
+{
+
+int getNumDiv(int i)
+{
+    int count = 0;
+    for (int j = 0; j<N; j++) // for loop that has code go through each element in array
+    {
+      if (array [i] % array [j] == 0) // if statement to have i check if it has no remainder from j and if it needs to add 1 to it's count
+      {
+        count++; // adds 1 to count if prior requirements are filled
+      }
+    }
+    return count;
+}
