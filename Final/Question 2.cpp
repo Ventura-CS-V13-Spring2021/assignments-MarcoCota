732c2a2592163b8d248c29885f0f23818d6fc965
MarcoCota <>
2021-05-17 07:45:38 +0000

    cont question 2

--- Final/Question 2.cpp
+++ Final/Question 2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -8,3 +9,11 @@ int main()
   int i; 
   cont int N = 20; // sets array to 20 elements max
   int array[20] = {-12, 3, -12, 4, 1, 1, -12, 1, -1, 1, 2, 3, 4, 2, 3, -12, 4, 2, 1, 5};  
+  sort(array, array + N, greater<int>()); // sorts elements in array from largest to smallest
+    
+    for (int i = 0; i<N; i++) // goes through each element in array
+    {
+        
+    }
+    
+}
