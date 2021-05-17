b8355b1b1c71559e23b273ea10871dc6573b1c04
MarcoCota <>
2021-05-17 08:50:23 +0000

    Cont question 3

--- Final/Question 3.cpp
+++ Final/Question 3.cpp
@@ -7,3 +7,10 @@ Student::Student(string n, int num)
     name = n;
     num_classes = num;
     class_list = new string[num];
+    for (int i = 0; i < num_classes; i++)
+    {
+        cout << "Enter the course name : ";
+        cin >> class_list[i];
+    }
+}
+
